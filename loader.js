6f24d40692399bc51a430e70691238085d1871b3
Ivan <denatarelcom2105@gmail.com>
2024-03-29 23:27:38 +0200

    add pm2 for error handling

--- loader.js
+++ loader.js
@@ -1,5 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 const config = require("./data/config.js");
+const fs = require("fs");
 
 const bot = new TelegramBot(config.BOT_TOKEN, {
   polling: true,
@@ -7,7 +8,12 @@ const bot = new TelegramBot(config.BOT_TOKEN, {
 });
 
 bot.on("polling_error", (error) => {
-  console.log(`Polling error: ${error.code} - ${error.message}`);
+  const errorMessage = `Polling error: ${error.code} - ${error.message}\n`;
+  fs.appendFile("error.txt", errorMessage, (err) => {
+    if (err) {
+      console.error("Ошибка при записи в файл:", err);
+    }
+  });
 });
 
 module.exports = { bot };
